69f4826ccc5e8e13e6cd6ee86de84a2ae07dd05a
Andrey Zakaryan <andreizakaryan@gmail.com>
2025-02-11 02:14:54 +0400

    Fix scrapers main

--- scrapers/main.py
+++ scrapers/main.py
@@ -5,17 +5,21 @@ from parsers import laevitas, amberdata, deribit, laevitas_funding, binance, alt
 
 async def main():
     # Initialize scheduler
+    print("Set Up")
+
     scheduler = AsyncIOScheduler()
     instruments = ["btc", "eth"]
-    # for instrument in instruments:
-    #     scheduler.add_job(laevitas.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
-    #     scheduler.add_job(amberdata.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
-    #     scheduler.add_job(deribit.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
-    #     scheduler.add_job(binance.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
-    #     scheduler.add_job(laevitas_funding.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
+    for instrument in instruments:
+        scheduler.add_job(laevitas.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
+        scheduler.add_job(amberdata.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
+        scheduler.add_job(deribit.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
+        scheduler.add_job(binance.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
+        scheduler.add_job(laevitas_funding.fetch_data, "interval", minutes=1, args=[instrument], max_instances=1)
 
     scheduler.add_job(alternative.fetch_data, "interval", minutes=1, max_instances=1)
     scheduler.start()
+
+    print("Scheduler Started")
 
     try:
         while True:
